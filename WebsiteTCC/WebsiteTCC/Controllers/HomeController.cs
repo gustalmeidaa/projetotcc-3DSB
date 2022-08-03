89b7f4a0bb6ff60ea0f2a51723394973b4f9fdc7
gustalmeidaa <gustavoheubel@gmail.com>
2022-08-03 17:02:11 -0300

    Website do TCC - v.0.0.5 Teste do Firebase no C#, testando a chamada de uma das receitas e mostrando na view "Receitas".

--- WebsiteTCC/WebsiteTCC/Controllers/HomeController.cs
+++ WebsiteTCC/WebsiteTCC/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,12 +16,21 @@ namespace WebsiteTCC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        IFirebaseClient cliente;
+        public HomeController()
         {
-            _logger = logger;
+            IFirebaseConfig conf = new FirebaseConfig()
+            {
+                AuthSecret = "1BMaJSOs6XlJViTkCgN9qikBSWwLKJsrnGNzohYP",
+                BasePath = "https://testefirebase-faa05-default-rtdb.firebaseio.com/"
+            };
+
+            cliente = new FirebaseClient(conf);
         }
+
+        
+
+        private readonly ILogger<HomeController> _logger;
 
         public IActionResult Index()
         {
@@ -25,7 +39,16 @@ namespace WebsiteTCC.Controllers
 
         public IActionResult Receitas()
         {
-            return View();
+            List<Receitas> listaReceitas = new List<Receitas>();
+
+            FirebaseResponse response = cliente.Get("receitas/1");
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Receitas r = JsonConvert.DeserializeObject<Receitas>(response.Body);
+                listaReceitas.Add(r);
+            }
+
+            return View(listaReceitas);
         }
         public IActionResult Login()
         {
---
+++ WebsiteTCC/WebsiteTCC/Models/Receitas.cs
@@ -0,0 +1,46 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteTCC.Models
+{
+    public class Receitas
+    {
+        public string nome;
+        //public string secao;
+        //public string conteudo;
+        public string[] ingredientes;
+        public string[] modoPreparo;
+
+
+        public string Nome
+        {
+            get => nome;
+            set => nome = value;
+        }
+        //public string Secao
+        //{
+        //    get => secao;
+        //    set => secao = value;
+        //}
+        //public string Conteudo
+        //{
+        //    get => conteudo;
+        //    set => conteudo = value;
+        //}
+        public string[] Ingredientes
+        {
+            get => ingredientes;
+            set => ingredientes = value;
+        }
+        public string[] ModoPreparo
+        {
+            get => modoPreparo;
+            set => modoPreparo = value;
+        }
+
+    }
+}
