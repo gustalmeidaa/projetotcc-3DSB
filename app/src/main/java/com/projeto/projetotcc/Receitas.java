2a8cddfaf1d64a89be9caeeba339dcfe00c34e04
gustalmeidaa <gustavoheubel@gmail.com>
2022-09-02 19:55:18 -0300

    App Android - v.0.0.4 Aplicativo para o projeto de TCC do SENAI Zerbini; Receitas sendo filtradas com base nos ingredientes fornecidos, nas próximas etapas, ao clicar no nome de uma receita, uma nova janela irá ser aberta, contando com todos os ingredientes necessários e o modo de preparo da mesma. Também foi criada uma nova classe "Receita", utilizada para o método de pesquisa das receitas.

--- app/src/main/java/com/projeto/projetotcc/Receitas.java
+++ app/src/main/java/com/projeto/projetotcc/Receitas.java
@@ -1,10 +1,17 @@
 package com.projeto.projetotcc;
 
+import static android.graphics.Color.rgb;
+
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.ColorStateListDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +31,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -35,14 +43,18 @@ public class Receitas extends Fragment {
     View view;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private List<String> listaIngredientes = new ArrayList<>();
+    private List<Receita> lReceitas = new ArrayList<>();
     private ChipGroup grupoIngredientes;
+
+
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
 
-    public Receitas() {}
+    public Receitas() {
+    }
 
     public static Receitas newInstance(String param1, String param2) {
         Receitas fragment = new Receitas();
@@ -110,6 +122,8 @@ public class Receitas extends Fragment {
             Chip chip = new Chip(getContext());
             chip.setText(ingredientes);
             chip.setChipIconResource(R.drawable.ic_receitas);
+            chip.setTextColor(ColorStateList.valueOf(Color.BLACK));
+            chip.setChipBackgroundColor(ColorStateList.valueOf(Color.rgb(255, 183, 2)));
             chip.setCloseIconVisible(true);
             chip.setX(30);
             chip.setOnCloseIconClickListener(new View.OnClickListener() {
@@ -133,23 +147,28 @@ public class Receitas extends Fragment {
                 db.collection("receitas").orderBy("ingredientes").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     ListView listaReceitas = view.findViewById(R.id.listaReceitas);
                     ArrayAdapter<String> adapter;
-                    private List<String> nomesReceitas = new ArrayList<>();
+                    List<Receita> listaDasReceitas;
+                    List<String> nomesReceitas = new ArrayList<>();
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         try {
                             if (!task.getResult().isEmpty()) {
-                                String ingredientes = "";
                                 for (QueryDocumentSnapshot document : task.getResult()) {
-                                    String campoIngredientes = document.get("ingredientes").toString();
-                                    for (String teste : Receitas.this.listaIngredientes) {
-                                        ingredientes = teste;
-                                        Toast.makeText(getContext(), "" + ingredientes, Toast.LENGTH_SHORT).show();
-                                        if (campoIngredientes.contains(ingredientes)) {
-                                            String nomes = document.get("nome").toString();
-                                            nomesReceitas.add(nomes);
-                                        }
-                                    }
+                                    Receita receitas = document.toObject(Receita.class);
+                                    lReceitas.add(receitas);
                                 }
+                                for(int i = 0; i < grupoIngredientes.getChildCount(); i++){
+                                    Chip chip = (Chip) grupoIngredientes.getChildAt(i);
+                                    String comparacao = chip.getText().toString();
+                                    listaDasReceitas = lReceitas.stream().filter(ingredientes -> ingredientes.ingredientes.stream().anyMatch(s -> s.contains(comparacao)))
+                                            .collect(Collectors.toList());
+                                    lReceitas = listaDasReceitas;
+                                }
+
+                                for (Receita rec : listaDasReceitas) {
+                                    nomesReceitas.add(rec.nome);
+                                }
+
                                 //Inicialização do adapter e atribuir a lista dos nomes
                                 adapter = new ArrayAdapter<String>(getContext(),
                                         android.R.layout.simple_list_item_1,
@@ -158,12 +177,14 @@ public class Receitas extends Fragment {
                                 //Adicionar os itens na lista usando o método setAdapter
                                 listaReceitas.setAdapter(adapter);
 
-                                //Limpando a lista após a finalização do método
+                                //Limpando a lista de ingredientes após a finalização do método
                                 listaIngredientes.clear();
-
+                                //Limpando a lista de receitas após a pesquisa
+                                lReceitas.clear();
                             } else {
-                                Toast.makeText(view.getContext().getApplicationContext(), "Deu ruim", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(view.getContext().getApplicationContext(), "Insira ao menos 1 (um) ingrediente", Toast.LENGTH_SHORT).show();
                             }
+
                         } catch (Exception ex) {
                             Toast.makeText(view.getContext().getApplicationContext(), "" + ex, Toast.LENGTH_SHORT).show();
                         }
