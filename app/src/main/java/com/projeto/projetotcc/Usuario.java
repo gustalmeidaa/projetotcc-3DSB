1be60f64c5f2013af5687f7ec57615dd0ac5a74f
gustalmeidaa <gustavoheubel@gmail.com>
2022-10-07 19:07:28 -0300

    App Android - v.0.0.7 Aplicativo para o projeto de TCC do SENAI Zerbini; Nesta versão, foi corrigido um bug que permitia que uma receita fosse pesquisada mesmo se ela estivesse na lista de alergênicos do usuário. Além disso, deu-se início a tela de pesquisa de receitas através do nome inserido pelo usuário (ainda em construção).

---
+++ app/src/main/java/com/projeto/projetotcc/Usuario.java
@@ -0,0 +1,44 @@
+package com.projeto.projetotcc;
+
+import java.util.List;
+
+public class Usuario {
+
+
+    private String nome;
+    private String email;
+    private List<String> alergenicos;
+
+
+    public Usuario(){}
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getAlergenicos() {
+        return alergenicos;
+    }
+
+    public void setAlergenicos(List<String> alergenicos) {
+        this.alergenicos = alergenicos;
+    }
+}
